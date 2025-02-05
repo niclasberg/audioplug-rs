ece922bd808ee897d66cd586522d91853358a17c
Niclas Berg <niclasbrg@gmail.com>
2025-02-05 16:46:54 +0100

    Removed unused objc declarations

--- src/platform/mac/audio_toolbox/mod.rs
+++
@@ -1,7 +0,0 @@
-mod audio_component;
-mod audio_unit_factory;
-mod audio_unit_bus_array;
-pub use audio_component::{AudioComponentDescription, AudioComponentFlags};
-pub use audio_unit_factory::*;
-pub use audio_unit_bus_array::*;
-pub(super) use super::cf_enum;
